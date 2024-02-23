37d8b6758931ac99d1ca22d537cf85f5feac5eb4
walyson <walysonmoises1342@gmail.com>
2024-02-22 23:27:08 -0300

    block requests by ip

--- redisPoc/src/main/java/com/waly/redisPoc/controller/ProductController.java
+++ redisPoc/src/main/java/com/waly/redisPoc/controller/ProductController.java
@@ -4,10 +4,18 @@ import com.waly.redisPoc.dto.ProductDTO;
 import com.waly.redisPoc.entities.Product;
 import com.waly.redisPoc.repository.ProductRepository;
 import com.waly.redisPoc.service.ProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.antlr.v4.runtime.misc.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RestController
@@ -16,10 +24,43 @@ public class ProductController {
 
     @Autowired
     private ProductService service;
+    private final ConcurrentHashMap<String, Pair<AtomicInteger, Instant>> requestCounts = new ConcurrentHashMap<>();
+
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> findById(@PathVariable int id){
-        log.info("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx " + String.valueOf(id));
+    public ResponseEntity<ProductDTO> findById(@PathVariable int id, HttpServletRequest request){
+        String clientIp = getClientIP(request);
+        log.info("IP " + clientIp);
+        Pair<AtomicInteger, Instant> pair = requestCounts.computeIfAbsent(clientIp, k -> new Pair<>(new AtomicInteger(), Instant.now()));
+        AtomicInteger count = pair.a;
+        long sinceLastrequest = Duration.between(pair.b, Instant.now()).getSeconds();
+
+        if(sinceLastrequest > 10){
+            count.set(0);
+            requestCounts.put(clientIp, new Pair<>(count, Instant.now()));
+        }
+        if (count.incrementAndGet() > 3){
+            return ResponseEntity.badRequest().build();
+        }
+        log.info(String.valueOf(count.get()));
         return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    private String getClientIP(HttpServletRequest request) {
+        String clientIP = request.getHeader("X-Forwarded-For");
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (clientIP == null || clientIP.isEmpty() || "unknown".equalsIgnoreCase(clientIP)) {
+            clientIP = request.getRemoteAddr();
+        }
+
+        return clientIP;
     }
 }
